03711b822d31ef35e13303e6ce15612d275c7b65
Kotso <constantine.chichanov@gmail.com>
2024-11-11 07:47:35 +0200

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CreditController;
+use App\Http\Controllers\PaymentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/credit', [CreditController::class, 'index'])->name('credits.index');
+Route::post('/credit', [CreditController::class, 'store'])->name('credit.store');
+
+Route::get('/payment', [PaymentController::class, 'index'])->name('payment.index');
+Route::post('/payment', [PaymentController::class, 'store'])->name('payment.store');
